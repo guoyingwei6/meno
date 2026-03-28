786d77b2a57ea91284ee1a6b39fb29fd0c90327e
YingweiGuo <guoyingwei6@outlook.com>
2026-03-28 09:25:59 +0800

    feat: slug format changed to YYYYMMDD-random6

--- worker/src/lib/slug.ts
+++ worker/src/lib/slug.ts
@@ -1,5 +1,8 @@
 import { customAlphabet } from 'nanoid';
 
-const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789', 10);
+const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789', 6);
 
-export const createMemoSlug = () => nanoid();
+export const createMemoSlug = () => {
+  const date = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+  return `${date}-${nanoid()}`;
+};
